bd7f0b8e1a22af37a10e66015c3d3d8a3f9fa005
BGirginn <girginbora30@gmail.com>
2025-08-05 01:51:07 +0300

    update

---
+++ chapter_2/src/main.rs
@@ -0,0 +1,11 @@
+// Owenership -> Stack and Heap
+// Referrence and borrowing
+// Slices
+fn main() {
+
+    //&str ile string arasındaki fark String e atanan yazı push vesaire ile dinamik olarak değiştirilebilirken &str sabittir ve değiştirilemez.
+    //&str de değişkene veri atanmış olmalı ancak String ile dışarıdan veri alınımı yapılabilir. &str ile input yapılamaz
+    
+
+    println!("Hello, world!");
+}
